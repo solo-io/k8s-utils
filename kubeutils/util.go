4ea84d7ad649a7c1ccb5ffd050989c1dd277e9e1
ashleywang1 <ashley.wang@solo.io>
2021-01-27 17:04:55 -0500

    sanitizeNameV3

--- kubeutils/util.go
+++ kubeutils/util.go
@@ -34,6 +34,28 @@ func SanitizeNameV2(name string) string {
 	name = strings.Replace(name, "[", "", -1)
 	name = strings.Replace(name, "]", "", -1)
 	name = strings.Replace(name, ":", "-", -1)
+	name = strings.Replace(name, " ", "-", -1)
+	name = strings.Replace(name, "\n", "", -1)
+	name = strings.Replace(name, "\"", "", -1)
+	name = strings.Replace(name, "'", "", -1)
+	if len(name) > 63 {
+		hash := md5.Sum([]byte(name))
+		name = fmt.Sprintf("%s-%x", name[:31], hash)
+		name = name[:63]
+	}
+	name = strings.Replace(name, ".", "-", -1)
+	name = strings.ToLower(name)
+	return name
+}
+
+
+func SanitizeNameV3(name string) string {
+	name = strings.Replace(name, "*", "-", -1)
+	name = strings.Replace(name, "/", "-", -1)
+	name = strings.Replace(name, ".", "-", -1)
+	name = strings.Replace(name, "[", "", -1)
+	name = strings.Replace(name, "]", "", -1)
+	name = strings.Replace(name, ":", "-", -1)
 	name = strings.Replace(name, "_", "-", -1)
 	name = strings.Replace(name, " ", "-", -1)
 	name = strings.Replace(name, "\n", "", -1)

5d1b69f87249be7bc9b0289c60c3e8dbeabac784
Keerthan Ekbote <keerthan.ekbote@solo.io>
2022-04-04 13:02:19 -0400

    upgrade crd version

--- kubeinstallutils/crd.go
+++ kubeinstallutils/crd.go
@@ -3,21 +3,22 @@ package kubeinstallutils
 import (
 	"context"
 
+	apiv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+
 	"github.com/rotisserie/eris"
-	"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	apiexts "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func CrdsFromManifest(crdManifestYaml string) ([]*v1beta1.CustomResourceDefinition, error) {
-	var crds []*v1beta1.CustomResourceDefinition
+func CrdsFromManifest(crdManifestYaml string) ([]*apiv1.CustomResourceDefinition, error) {
+	var crds []*apiv1.CustomResourceDefinition
 	crdRuntimeObjects, err := ParseKubeManifest(crdManifestYaml)
 	if err != nil {
 		return nil, err
 	}
 	for _, obj := range crdRuntimeObjects {
-		apiExtCrd, ok := obj.(*v1beta1.CustomResourceDefinition)
+		apiExtCrd, ok := obj.(*apiv1.CustomResourceDefinition)
 		if !ok {
 			return nil, eris.Wrap(err, "internal error: crd manifest must only contain CustomResourceDefinitions")
 		}
@@ -26,9 +27,9 @@ func CrdsFromManifest(crdManifestYaml string) ([]*v1beta1.CustomResourceDefiniti
 	return crds, nil
 }
 
-func CreateCrds(ctx context.Context, apiExts apiexts.Interface, crds ...*v1beta1.CustomResourceDefinition) error {
+func CreateCrds(ctx context.Context, apiExts apiexts.Interface, crds ...*apiv1.CustomResourceDefinition) error {
 	for _, crd := range crds {
-		if _, err := apiExts.ApiextensionsV1beta1().CustomResourceDefinitions().Create(ctx, crd, v1.CreateOptions{}); err != nil && !apierrors.IsAlreadyExists(err) {
+		if _, err := apiExts.ApiextensionsV1().CustomResourceDefinitions().Create(ctx, crd, v1.CreateOptions{}); err != nil && !apierrors.IsAlreadyExists(err) {
 			return eris.Wrapf(err, "failed to create crd: %v", crd)
 		}
 	}
@@ -37,7 +38,7 @@ func CreateCrds(ctx context.Context, apiExts apiexts.Interface, crds ...*v1beta1
 
 func DeleteCrds(ctx context.Context, apiExts apiexts.Interface, crdNames ...string) error {
 	for _, name := range crdNames {
-		err := apiExts.ApiextensionsV1beta1().CustomResourceDefinitions().Delete(ctx, name, v1.DeleteOptions{})
+		err := apiExts.ApiextensionsV1().CustomResourceDefinitions().Delete(ctx, name, v1.DeleteOptions{})
 		if err != nil && !apierrors.IsNotFound(err) {
 			return eris.Wrapf(err, "failed to delete crd: %v", name)
 		}
--- kubeutils/wait_crd_test.go
+++ kubeutils/wait_crd_test.go
@@ -3,10 +3,11 @@ package kubeutils_test
 import (
 	"context"
 
+	apiv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	. "github.com/solo-io/k8s-utils/kubeutils"
-	"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	apiexts "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -23,23 +24,27 @@ var _ = Describe("WaitCrd", func() {
 		Expect(err).NotTo(HaveOccurred())
 		api, err = apiexts.NewForConfig(cfg)
 		Expect(err).NotTo(HaveOccurred())
-		crd, err := api.ApiextensionsV1beta1().CustomResourceDefinitions().Create(ctx, &v1beta1.CustomResourceDefinition{
+		crd, err := api.ApiextensionsV1().CustomResourceDefinitions().Create(ctx, &apiv1.CustomResourceDefinition{
 			ObjectMeta: v1.ObjectMeta{Name: "somethings.test.solo.io"},
-			Spec: v1beta1.CustomResourceDefinitionSpec{
+			Spec: apiv1.CustomResourceDefinitionSpec{
 				Group: "test.solo.io",
-				Names: v1beta1.CustomResourceDefinitionNames{
+				Names: apiv1.CustomResourceDefinitionNames{
 					Plural:     "somethings",
 					Kind:       "Something",
 					ShortNames: []string{"st"},
 				},
-				Version: "v1",
+				Versions: []apiv1.CustomResourceDefinitionVersion{
+					{
+						Name: "v1",
+					},
+				},
 			},
 		}, v1.CreateOptions{})
 		Expect(err).NotTo(HaveOccurred())
 		crdName = crd.Name
 	})
 	AfterEach(func() {
-		api.ApiextensionsV1beta1().CustomResourceDefinitions().Delete(ctx, crdName, v1.DeleteOptions{})
+		api.ApiextensionsV1().CustomResourceDefinitions().Delete(ctx, crdName, v1.DeleteOptions{})
 	})
 	It("waits successfully for a crd to become established", func() {
 		err := WaitForCrdActive(ctx, api, crdName)

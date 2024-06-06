3e9358d4e45f05e8ec6e74d98ec53fb9e9ad891b
Jacob Bohanon <jacob.bohanon@solo.io>
2024-06-06 16:59:58 -0400

    add generic exec in ephemeral tool

--- testutils/kube/curl.go
+++ testutils/kube/curl.go
@@ -1,64 +1,36 @@
 package kube
 
 import (
-	"bytes"
 	"context"
 	"fmt"
 	"io"
-	"io/ioutil"
 
-	"github.com/solo-io/go-utils/testutils"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 
 	. "github.com/onsi/gomega"
+	"github.com/solo-io/go-utils/testutils/kubectl"
 )
 
-func CurlWithEphemeralPod(ctx context.Context, logger io.Writer, kubecontext, fromns, frompod string, args ...string) string {
-	createargs := []string{"alpha", "debug", "--quiet",
-		"--image=curlimages/curl@sha256:aa45e9d93122a3cfdf8d7de272e2798ea63733eeee6d06bd2ee4f2f8c4027d7c",
-		"--container=curl", frompod, "-n", fromns, "--", "sleep", "10h"}
-	// Execute curl commands from the same pod each time to avoid creating a burdensome number of ephemeral pods.
-	// create the curl pod; we do this every time and it will only work the first time, so ignore failures
-	executeNoFail(ctx, logger, kubecontext, createargs...)
-	args = append([]string{"exec",
-		"--container=curl", frompod, "-n", fromns, "--", "curl", "--connect-timeout", "1", "--max-time", "5"}, args...)
-	return execute(ctx, logger, kubecontext, args...)
-}
-
-func CurlWithEphemeralPodStable(ctx context.Context, logger io.Writer, kubeContext, fromNs, fromPod string, args ...string) string {
-	createArgs := []string{
-		"debug",
-		"--quiet",
-		"--image=curlimages/curl@sha256:aa45e9d93122a3cfdf8d7de272e2798ea63733eeee6d06bd2ee4f2f8c4027d7c",
-		"--container=curl",
-		"--image-pull-policy=IfNotPresent",
-		fromPod,
-		"-n",
-		fromNs,
-		"--",
-		"sleep",
-		"10h",
+func CurlWithEphemeralPod(ctx context.Context, logger io.Writer, kubeContext, fromns, frompod string, args ...string) string {
+	execParams := EphemeralPodParams{
+		Logger:      logger,
+		KubeContext: kubeContext,
+		Image: Image{
+			Registry:   "curlimages",
+			Repository: "curl",
+			Tag:        "",
+			Digest:     "aa45e9d93122a3cfdf8d7de272e2798ea63733eeee6d06bd2ee4f2f8c4027d7c",
+			PullPolicy: "IfNotPresent",
+		},
+		FromContainer: "curl",
+		FromNamespace: fromns,
+		FromPod:       frompod,
+		ExecCmdPath:   "curl",
+		Args:          args,
 	}
-	// Execute curl commands from the same pod each time to avoid creating a burdensome number of ephemeral pods.
-	// create the curl pod; we do this every time and it will only work the first time, so ignore failures
-	_, _ = executeNoFail(ctx, logger, kubeContext, createArgs...)
-
-	args = append([]string{
-		"exec",
-		"--container=curl",
-		fromPod,
-		"-n",
-		fromNs,
-		"--",
-		"curl",
-		"--connect-timeout",
-		"1",
-		"--max-time",
-		"5",
-	}, args...)
-	return execute(ctx, logger, kubeContext, args...)
+	return ExecFromEphemeralPod(ctx, execParams)
 }
 
 // labelSelector is a string map e.g. gloo=gateway-proxy
@@ -71,36 +43,56 @@ func FindPodNameByLabel(cfg *rest.Config, ctx context.Context, ns, labelSelector
 	return pl.Items[0].GetName()
 }
 
-func WaitForRollout(ctx context.Context, logger io.Writer, kubecontext, ns, deployment string) {
+func WaitForRollout(ctx context.Context, logger io.Writer, kubeContext, ns, deployment string) {
 	args := []string{"-n", ns, "rollout", "status", "deployment", deployment}
-	execute(ctx, logger, kubecontext, args...)
+	mustExecute(ctx, KubectlParams{
+		KubectlCmdParams: kubectl.NewParams(args...),
+		KubeContext:      kubeContext,
+		Logger:           logger,
+	})
 }
 
-func Curl(ctx context.Context, logger io.Writer, kubecontext, ns, fromDeployment, fromContainer, url string) string {
+func Curl(ctx context.Context, logger io.Writer, kubeContext, ns, fromDeployment, fromContainer, url string) string {
 	args := []string{
 		"-n", ns,
 		"exec", fmt.Sprintf("deployment/%s", fromDeployment),
 		"-c", fromContainer,
 		"--", "curl", url,
 	}
-	return execute(ctx, logger, kubecontext, args...)
+	return mustExecute(ctx, KubectlParams{
+		KubectlCmdParams: kubectl.NewParams(args...),
+		KubeContext:      kubeContext,
+		Logger:           logger,
+	})
 }
 
 func CreateNamespace(ctx context.Context, logger io.Writer, kubeContext, ns string) {
 	args := []string{"create", "namespace", ns}
-	out := execute(ctx, logger, kubeContext, args...)
+	out := mustExecute(ctx, KubectlParams{
+		KubectlCmdParams: kubectl.NewParams(args...),
+		KubeContext:      kubeContext,
+		Logger:           logger,
+	})
 	fmt.Fprintln(logger, out)
 }
 
 func DeleteNamespace(ctx context.Context, logger io.Writer, kubeContext, ns string) {
 	args := []string{"delete", "namespace", ns}
-	out := execute(ctx, logger, kubeContext, args...)
+	out := mustExecute(ctx, KubectlParams{
+		KubectlCmdParams: kubectl.NewParams(args...),
+		KubeContext:      kubeContext,
+		Logger:           logger,
+	})
 	fmt.Fprintln(logger, out)
 }
 
 func LabelNamespace(ctx context.Context, logger io.Writer, kubeContext, ns, label string) {
 	args := []string{"label", "namespace", ns, label}
-	out := execute(ctx, logger, kubeContext, args...)
+	out := mustExecute(ctx, KubectlParams{
+		KubectlCmdParams: kubectl.NewParams(args...),
+		KubeContext:      kubeContext,
+		Logger:           logger,
+	})
 	fmt.Fprintln(logger, out)
 }
 
@@ -118,7 +110,11 @@ func SetDeploymentEnvVars(
 		envVarStrings = append(envVarStrings, fmt.Sprintf("%s=%s", name, value))
 	}
 	args := append([]string{"set", "env", "-n", ns, fmt.Sprintf("deployment/%s", deploymentName), "-c", containerName}, envVarStrings...)
-	out := execute(ctx, logger, kubeContext, args...)
+	out := mustExecute(ctx, KubectlParams{
+		KubectlCmdParams: kubectl.NewParams(args...),
+		KubeContext:      kubeContext,
+		Logger:           logger,
+	})
 	fmt.Fprintln(logger, out)
 }
 
@@ -130,14 +126,18 @@ func DisableContainer(
 	deploymentName string,
 	containerName string,
 ) {
-	args := append([]string{
+	args := []string{
 		"-n", ns,
 		"patch", "deployment", deploymentName,
 		"--patch",
-		fmt.Sprintf("{\"spec\": {\"template\": {\"spec\": {\"containers\": [{\"name\": \"%s\",\"command\": [\"sleep\", \"20h\"]}]}}}}",
+		fmt.Sprintf(`{"spec": {"template": {"spec": {"containers": [{"name": "%s","command": ["sleep", "20h"]}]}}}}`,
 			containerName),
+	}
+	out := mustExecute(ctx, KubectlParams{
+		KubectlCmdParams: kubectl.NewParams(args...),
+		KubeContext:      kubeContext,
+		Logger:           logger,
 	})
-	out := execute(ctx, logger, kubeContext, args...)
 	fmt.Fprintln(logger, out)
 }
 
@@ -148,39 +148,16 @@ func EnableContainer(
 	ns string,
 	deploymentName string,
 ) {
-	args := append([]string{
+	args := []string{
 		"-n", ns,
 		"patch", "deployment", deploymentName,
 		"--type", "json",
-		"-p", "[{\"op\": \"remove\", \"path\": \"/spec/template/spec/containers/0/command\"}]",
+		"-p", `[{"op": "remove", "path": "/spec/template/spec/containers/0/command"}]`,
+	}
+	out := mustExecute(ctx, KubectlParams{
+		KubectlCmdParams: kubectl.NewParams(args...),
+		KubeContext:      kubeContext,
+		Logger:           logger,
 	})
-	out := execute(ctx, logger, kubeContext, args...)
 	fmt.Fprintln(logger, out)
-}
-
-func execute(ctx context.Context, logger io.Writer, kubeContext string, args ...string) string {
-	data, err := executeNoFail(ctx, logger, kubeContext, args...)
-	Expect(err).NotTo(HaveOccurred())
-	return data
-}
-
-func executeNoFail(ctx context.Context, logger io.Writer, kubeContext string, args ...string) (string, error) {
-	args = append([]string{"--context", kubeContext}, args...)
-	fmt.Fprintf(logger, "Executing: kubectl %v \n", args)
-	readerChan, done, err := testutils.KubectlOutChan(&bytes.Buffer{}, args...)
-	if err != nil {
-		return "", err
-	}
-	defer close(done)
-	select {
-	case <-ctx.Done():
-		return "", nil
-	case reader := <-readerChan:
-		data, err := ioutil.ReadAll(reader)
-		if err != nil {
-			return "", err
-		}
-		fmt.Fprintf(logger, "<kubectl %v> output: %v\n", args, string(data))
-		return string(data), nil
-	}
 }
---
+++ testutils/kube/exec.go
@@ -0,0 +1,92 @@
+package kube
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"strings"
+
+	"github.com/solo-io/go-utils/testutils/kubectl"
+)
+
+type Image struct {
+	Registry, Repository, Tag, Digest string
+	PullPolicy                        string
+}
+
+func (i Image) String() string {
+	b := strings.Builder{}
+	b.WriteString(i.Registry)
+	b.WriteRune('/')
+	b.WriteString(i.Repository)
+	if i.Tag != "" {
+		b.WriteRune(':')
+		b.WriteString(i.Tag)
+	}
+	if i.Digest != "" {
+		b.WriteString("@sha256:")
+		b.WriteString(i.Digest)
+	}
+
+	return b.String()
+}
+
+type EphemeralPodParams struct {
+	Logger        io.Writer
+	KubeContext   string
+	Image         Image
+	FromContainer string
+	FromNamespace string
+	FromPod       string
+	ExecCmdPath   string
+	Args          []string
+	Env           []string
+	Stdin         io.Reader
+	Stdout        io.Writer
+	Stderr        io.Writer
+}
+
+func ExecFromEphemeralPod(ctx context.Context, params EphemeralPodParams) string {
+	createargs := []string{
+		"debug",
+		"--quiet",
+		params.Image.String(),
+		fmt.Sprintf("--container=%s", params.FromContainer),
+		fmt.Sprintf("--image-pull-policy=%s", params.Image.PullPolicy),
+		params.FromPod,
+		"-n", params.FromNamespace,
+		"--", "sleep", "10h",
+	}
+
+	// create the params to the kubectl commands we will invoke.
+	// we will use the same params but just switch out the args for the
+	// different commands we execute.
+	kParams := kubectl.NewParams(createargs...)
+	kParams.Stdin = params.Stdin
+	kParams.Stdout = params.Stdout
+	kParams.Stderr = params.Stderr
+	kParams.Env = params.Env
+
+	// Execute curl commands from the same pod each time to avoid creating a burdensome number of ephemeral pods.
+	// create the curl pod; we do this every time and it will only work the first time, so ignore failures
+	_, _ = execute(ctx, KubectlParams{
+		KubectlCmdParams: kParams,
+		KubeContext:      params.KubeContext,
+		Logger:           params.Logger,
+	})
+
+	execArgs := []string{
+		"exec",
+		fmt.Sprintf("--container=%s", params.FromContainer),
+		params.FromPod,
+		"-n", params.FromNamespace,
+		"--", params.ExecCmdPath,
+	}
+
+	kParams.Args = append(execArgs, params.Args...)
+	return mustExecute(ctx, KubectlParams{
+		KubectlCmdParams: kParams,
+		KubeContext:      params.KubeContext,
+		Logger:           params.Logger,
+	})
+}
